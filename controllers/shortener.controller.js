29074812a938b62623de6ca75088184471d758a2
hackercaleb <130366225+hackercaleb@users.noreply.github.com>
2023-09-15 20:13:50 +0100

    worked on url shortener model

--- controllers/shortener.controller.js
+++ controllers/shortener.controller.js
@@ -1,24 +1,27 @@
 const ShortenedURL = require("./../models/shortener.model");
-
-exports.getUrls = async (req, res) => {
-  try {
-    const allurl = await ShortenedURL.find();
-
-    res.status(200).json({
-      status: "success",
-      data: {
-        allurl,
-      },
-    });
-  } catch (err) {
-    res.status(404).json({
-      status: "fail",
-      message: err,
-    });
-  }
-};
+const nanoid = require("nanoid");
+const { check } = require("express-validator");
 
 exports.createUrls = async (req, res) => {
+  /*//to validate url
+  const validateURL = check("url")
+    .notEmpty()
+    .withMessage("URL is required")
+    .isURL()
+    .withMessage("Invalid URL");
+
+  // Check if the URL already exists in the database
+  const existingURL = await ShortenedURL.findOne({ originalUrl: url });
+
+  if (existingURL) {
+    return res.status(200).json({
+      message: "Short URL already exists",
+      data: {
+        shortUrl: existingURL.shortUrl,
+      },
+    });
+  }*/
+  /*
   try {
     const newShortenedURL = await ShortenedURL.create(req.body);
 
@@ -33,5 +36,23 @@ exports.createUrls = async (req, res) => {
       status: "fail",
       message: err,
     });
-  }
+  }*/
+};
+
+exports.getUrls = async (req, res) => {
+  /*  try {
+    const allurl = await ShortenedURL.find();
+
+    res.status(200).json({
+      status: "success",
+      data: {
+        allurl,
+      },
+    });
+  } catch (err) {
+    res.status(404).json({
+      status: "fail",
+      message: err,
+    });
+  }*/
 };
--- index.js
+++ index.js
@@ -8,6 +8,7 @@ const app = express();
 const PORT = 3007;
 
 connectDB();
+app.use(express.json());
 
 app.use("/", routes);
 
